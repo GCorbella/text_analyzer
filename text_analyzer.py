d3c74aa5e001090e804b69dd0002396f7d4d63b5
Grifo55 <guillepcorbella@hotmail.com>
2022-06-02 14:20:21 +0200

    Inputs and variables created.

---
+++ text_analyzer.py
@@ -0,0 +1,4 @@
+text = input("Please, enter your text: ")
+letter1 = input("Please, introduce the first letter you want to search: ")
+letter2 = input("Please, introduce the second letter you want to search: ")
+letter3 = input("Please, introduce the third letter you want to search: ")
